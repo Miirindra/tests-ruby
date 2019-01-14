7994aeb06f94a3813d985ed695eef97620ec7e65
Hery Mirindra <hmirindra@gmail.com>
2019-01-14 20:12:19 +0300

    final version

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,18 +1,34 @@
-def who_is_bigger(a,b,c)
-	if a > b && a > c
-		puts "#{a} is a bigger"
-	elsif b > a && b > c
-		puts "#{b} is a bigger"
-	elsif c > a && c > b
-		puts "#{c} is a bigger"
-	else
-		puts "nil detected"
-	end
+def who_is_bigger(a, b, c)
+    if a == nil || b == nil || c == nil
+        return "nil detected"
+    else
+        if (a > b) && (a > c)
+            return "a is bigger"
+        elsif (b > a) && (b > c)
+            return "b is bigger"
+        else
+            return "c is bigger"
+        end
+    end
 end
 
-def reverse_upcase_noLTA(a)
-	a = a.upcase
-	a = a.reverse
+def reverse_upcase_noLTA(str)
+	str = str.downcase
+	str = str.delete "a"
+	str = str.delete "l"
+	str = str.delete "t"
+	str = str.upcase
+	str = str.reverse 
 end
 
-puts reverse_upcase_noLTA("vaohita")
+def array_42(array)
+  array.include?(42)
+end
+
+def magic_array(tab)
+	tab=tab
+	x = tab.select{|p|  p%2 == 0}
+	tabr = x.select{|p| p%3 == 0}
+	result = x - tabr
+	puts result.sort!	
+end
